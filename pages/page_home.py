fbf7d52cc013a9daddffc094cde538ec0ff5a5b4
kvpratama <kvpratama@gmail.com>
2025-07-02 17:09:10 +0900

    display recommended dishes

--- pages/page_home.py
+++ pages/page_home.py
@@ -1,13 +1,41 @@
 import streamlit as st
+import os
+import tempfile
+import logging
+import uuid
+from dishdecode.graph import graph
+
+# Configure logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
 
 st.title("Dish Decode")
 
 st.markdown("""
 This app helps you to decode Korean dish names.
 """)
-if 'thread_id_selfrag' not in st.session_state:
-    uploaded_files = st.file_uploader("Choose images", type=["png", "jpg", "jpeg", "heic", "heif"], accept_multiple_files=True)
+if 'thread_id_dishdecode' not in st.session_state:
+    uploaded_file = st.file_uploader("Choose images", type=["png", "jpg", "jpeg"], accept_multiple_files=False)
 
-    if uploaded_files:
-        for uploaded_file in uploaded_files:
-            st.image(uploaded_file)
+    if uploaded_file:
+        st.image(uploaded_file)
+        st.session_state['thread_id_dishdecode'] = str(uuid.uuid4())
+        with st.spinner("Processing..."):
+            paths = []
+            with tempfile.NamedTemporaryFile(delete=False, suffix=os.path.splitext(uploaded_file.name)[1]) as tmp:
+                logger.info(f"Saving temporary file: {tmp.name}")
+                tmp.write(uploaded_file.read())
+                paths.append(tmp.name)
+            result = graph.invoke(
+                {
+                    "image_path": paths[0],
+                    "max_size": 640,
+                },
+                # thread_id=st.session_state['thread_id_dishdecode']
+            )
+            for dish in result['recommended_dishes']:
+                st.write(dish.korean_name)
+                st.write(dish.english_name)
+                st.write(dish.description)
+                st.write(dish.why)
+                st.write("---")
